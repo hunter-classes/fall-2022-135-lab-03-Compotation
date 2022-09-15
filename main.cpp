7dc2415f6140773f083058dc46724f83dd70f8df
Compotation <46853874+Compotation@users.noreply.github.com>
2022-09-15 13:57:32 -0400

    max storage

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main() {
 	std::cout << "East basin storage: " << get_east_storage("02/20/2018") << " billion gallons" << std::endl;
 
 	std::cout << "minimum storage in East basin: " << get_min_east() << " billion gallons" << std::endl;
+	std::cout << "MAXimum storage in East basin: " << get_max_east() << " billion gallons" << std::endl;
   	return 0;
 }
--- reservoir.cpp
+++ reservoir.cpp
@@ -58,4 +58,27 @@ double get_min_east() {
 }
 
 double get_max_east() {
+	std::ifstream fin("Current_Reservoir_Levels.tsv");
+	if (fin.fail()) {
+		std::cerr << "File cannot be opened for reading." << std::endl;
+    		exit(1); // exit if failed to open the file
+	}
+	
+	std::string junk;
+	std::getline(fin, junk);
+
+	std::string filedate;
+	double eastSt;
+	double eastEl;
+	double westSt;
+	double westEl;
+	double maxStorage = -DBL_MAX;
+	while(fin >> filedate >> eastSt >> eastEl >> westSt >> westEl) {
+		fin.ignore(INT_MAX, '\n');
+		if (maxStorage < eastSt) {
+			maxStorage = eastSt;
+		}
+	}
+	fin.close();
+	return maxStorage;
 }
