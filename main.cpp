2bbd429256158cf8a7fec84f73402ba4e94c6f46
Compotation <46853874+Compotation@users.noreply.github.com>
2022-09-15 13:50:04 -0400

    print min storage

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@ int main() {
 	get_east_storage("01/01/2018");
 	get_east_storage("05/20/2018");
 	get_east_storage("02/20/2018");
+
+	std::cout << "minimum storage in East basin: " << get_min_east() << " billion gallons" << std::endl;
   	return 0;
 }
--- reservoir.cpp
+++ reservoir.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <climits>
 #include <cstring>
+#include <cfloat>
 
 double get_east_storage(std::string date) {
 	std::ifstream fin("Current_Reservoir_Levels.tsv");
@@ -32,6 +33,29 @@ double get_east_storage(std::string date) {
 }
 
 double get_min_east() {
+	std::ifstream fin("Current_Reservoir_Levels.tsv");
+	if (fin.fail()) {
+		std::cerr << "File cannot be opened for reading." << std::endl;
+    		exit(1); // exit if failed to open the file
+	}
+	
+	std::string junk;
+	std::getline(fin, junk);
+
+	std::string filedate;
+	double eastSt;
+	double eastEl;
+	double westSt;
+	double westEl;
+	double minStorage = DBL_MAX;
+	while(fin >> filedate >> eastSt >> eastEl >> westSt >> westEl) {
+		fin.ignore(INT_MAX, '\n');
+		if (minStorage > eastSt) {
+			minStorage = eastSt;
+		}
+	}
+	fin.close();
+	return minStorage;
 }
 
 double get_max_east() {
