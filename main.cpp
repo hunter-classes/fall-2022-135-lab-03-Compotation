d66bc67a67f6e5ea96a3e51bc67240da6a36d062
Compotation <46853874+Compotation@users.noreply.github.com>
2022-09-15 17:20:06 -0400

    mostly working task d except for last

--- main.cpp
+++ main.cpp
@@ -14,7 +14,14 @@ int main() {
 	std::cout << "09/14/2018 " << compare_basins("09/13/2018") << std::endl;
 	std::cout << "01/14/2018 " << compare_basins("09/13/2018") << std::endl;
 
-	reverse_order("01/01/2018", "03/01/2018");	
+	reverse_order("01/01/2018", "03/01/2018");
+    std::cout << std::endl << std::endl;
+    reverse_order("05/01/2018", "08/25/2018");
+    std::cout << std::endl << std::endl << "done" << std::endl;
+    // line below isn't working properly starts at 12/30/2018 and ends 1/02/2018
+    reverse_order("01/02/2018", "01/05/2018");
+    std::cout << std::endl << std::endl;
 
-  	return 0;
+
+    return 0;
 }
--- reverseorder.cpp
+++ reverseorder.cpp
@@ -1,5 +1,39 @@
 #include "reverseorder.h"
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+
 
 void reverse_order(std::string date1, std::string date2) {
+    std::ifstream fin("Current_Reservoir_Levels.tsv");
+    if (fin.is_open()) {
+        std::vector<std::string> reverselines;
+        std::string line;
+        while (std::getline(fin, line)) {
+            reverselines.insert(reverselines.begin(), line);
+        }
+        reverselines.erase(reverselines.end() - 1);
+
+        std::string filedate;
+        double eastSt;
+        double eastEl;
+        double westSt;
+        double westEl;
+        bool date2found;
+        for (std::string line : reverselines) {
+            std::stringstream linestream(line);
+            linestream >> filedate >> eastSt >> eastEl >> westSt >> westEl;
+            if (date2.compare(filedate) == 0) {
+                std::cout << filedate << " " << westEl << " ft" << std::endl;
+                date2found = true;
+            } else if (date1.compare(filedate) == 0) {
+                std::cout << filedate << " " << westEl << " ft" << std::endl;
+                return;
+            }  else if (date2found) {
+                std::cout << filedate << " " << westEl << " ft" << std::endl;
+            }
+        }
+    }
 }
