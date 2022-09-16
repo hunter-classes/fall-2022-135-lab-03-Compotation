8abde9d914865ae142e78c13593a66375ca7a3de
Compotation <46853874+Compotation@users.noreply.github.com>
2022-09-16 00:12:10 -0400

    spaces between outputs

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 int main() {
 	std::cout << "East basin storage: " << get_east_storage("01/01/2018") << " billion gallons" << std::endl;
 	std::cout << "East basin storage: " << get_east_storage("05/20/2018") << " billion gallons" << std::endl;
-	std::cout << "East basin storage: " << get_east_storage("02/20/2018") << " billion gallons" << std::endl;
+	std::cout << "East basin storage: " << get_east_storage("02/20/2018") << " billion gallons" << std::endl << std::endl;
 
 	std::cout << "minimum storage in East basin: " << get_min_east() << " billion gallons" << std::endl;
-	std::cout << "MAXimum storage in East basin: " << get_max_east() << " billion gallons" << std::endl;
+	std::cout << "MAXimum storage in East basin: " << get_max_east() << " billion gallons" << std::endl << std::endl;
 
 	std::cout << "09/13/2018 " << compare_basins("09/13/2018") << std::endl;
 	std::cout << "09/14/2018 " << compare_basins("09/13/2018") << std::endl;
